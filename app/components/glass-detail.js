481f57f237afa5ce84a42dc89a19480fde216b24
Ryan Tablada <ryan.tablada@gmail.com>
2015-08-25 14:10:47 -0500

    That route was weird and broken

--- app/components/glass-detail.js
+++ app/components/glass-detail.js
@@ -1,10 +1,20 @@
 import Ember from 'ember';
+import config from 'drink-it-up/config/environment';
+
+var ObjectPromiseProxy = Ember.ObjectProxy.extend(Ember.PromiseProxyMixin);
 
 export default Ember.Component.extend({
-  glass: null,
+  drink: null,
+  glassInfo: Ember.computed('drink.servedIn.id', function() {
+    var id = this.get('drink.servedIn.id');
+
+    return ObjectPromiseProxy.create({
+      promise: Ember.$.ajax(`${config.proxyHost}/glasses/${id}`)
+    });
+  }),
 
   glassPhotoUrl: Ember.computed('drink.servedIn.id', function() {
-    var id = this.get('glass.id');
+    var id = this.get('drink.servedIn.id');
 
     return `http://assets.absolutdrinks.com/glasses/${id}.jpg`;
   }),
--- app/routes/drinks/detail.js
+++ app/routes/drinks/detail.js
@@ -1,19 +1,8 @@
 import Ember from 'ember';
-import config from 'drink-it-up/config/environment';
 
 export default Ember.Route.extend({
   model(params) {
-    var drink = this.modelFor('drinks').result
+    return this.modelFor('drinks').result
       .findBy('id', params.drink_id);
-
-    return Ember.RSVP.hash({
-      drink: drink,
-      glass: Ember.$.ajax(`${config.proxyHost}/glasses/${drink.servedIn.id}`)
-    });
   },
-  setupController(controller, model) {
-    controller.set('glass', model.glass);
-
-    return this._super(controller, model.drink);
-  }
 });
